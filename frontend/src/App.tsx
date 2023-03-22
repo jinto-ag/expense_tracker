3f119078b3b1e0380b495e3299a26bc20bd0d786
Jinto AG <project.jintoag@gmail.com>
2023-03-22 11:52:42 +0530

    initial setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
